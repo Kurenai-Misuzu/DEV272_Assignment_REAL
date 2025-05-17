ebc2ad2150e6af25f548a85251fd09a4677037d2
Dylan Verallo <61432427+Kurenai-Misuzu@users.noreply.github.com>
2025-05-17 16:27:04 -0700

    tried new test

--- app/__tests__/index.test.tsx
+++ app/__tests__/index.test.tsx
@@ -2,11 +2,27 @@ import HomeScreen from "../(tabs)";
 
 import { render } from "@testing-library/react-native";
 
-import { useVenueListContext } from "@/context/VenueListContext";
+import { VenueListContextProvider } from "@/context/VenueListContext";
+import { VenueContextProvider } from "@/context/VenueContext";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NavigationContainer } from "@react-navigation/native";
+import { Stack } from "expo-router";
 
 describe('initial test of rendering homescreen', () => {
     test('Checks if text is rendered', () => {
-        const { getByText } = render(<HomeScreen />);
+
+        const queryClient = new QueryClient();
+
+
+        const { getByText } = render(
+            <QueryClientProvider client={queryClient}>
+                <VenueListContextProvider>
+                    <VenueContextProvider>
+                        <HomeScreen />
+                    </VenueContextProvider>
+                </VenueListContextProvider>
+            </QueryClientProvider>
+    );
 
         expect(getByText('Open Volleyball courts near you!')).toBeTruthy();
     });
